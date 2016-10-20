320e3d141f1d2893db751d8f1f01e87e5837b7ee
Felix Queißner <git@mq32.de>
2016-10-20 13:32:27 +0200

    Improves c backend.

--- EinCompiler/BackEnds/CCodeBackEnd.cs
+++ EinCompiler/BackEnds/CCodeBackEnd.cs
@@ -11,18 +11,34 @@ namespace EinCompiler.BackEnds
 	{
 		readonly Dispatcher<InstructionDescription, object> writeInstruction = new Dispatcher<InstructionDescription, object>();
 		readonly Dispatcher<Expression, object> writeExpression = new Dispatcher<Expression, object>();
+		readonly Dictionary<BinaryOperator, string> binaryOperators = new Dictionary<BinaryOperator, string> () {
+			{ BinaryOperator.Addition, "+" },
+			{ BinaryOperator.Subtraction, "-" },
+			{ BinaryOperator.Multiplication, "*" },
+			{ BinaryOperator.Division, "/" },
+			{ BinaryOperator.EuclideanDivision, "%" },
+			{ BinaryOperator.Equals, "==" },
+			{ BinaryOperator.Differs, "!=" },
+			{ BinaryOperator.GreaterOrEqual, ">=" },
+			{ BinaryOperator.GreaterThan, ">" },
+			{ BinaryOperator.LessOrEqual, "<=" },
+			{ BinaryOperator.LessThan, "<" },
+		};
 
 		public CCodeBackEnd()
 		{
 			writeInstruction.Register<ExpressionInstruction> (WriteExpressionInstruction);
-
-
+			writeInstruction.Register<ReturnInstruction> (WriteReturn);
+			writeInstruction.Register<ConditionalInstruction> (WriteIf);
+			writeInstruction.Register<LoopInstruction> (WriteLoop);
+			writeInstruction.Register<BreakLoopInstruction> (WriteBreak);
 
 			writeExpression.Register<AssignmentExpression> (WriteAssignment);
 			writeExpression.Register<VariableExpression> (WriteVariable);
 			writeExpression.Register<LiteralExpression> (WriteLiteral);
 			writeExpression.Register<FunctionCallExpression> (WriteCall);
 			writeExpression.Register<IndexerExpression> (WriteIndexer);
+			writeExpression.Register<BinaryOperatorExpression> (WriteBinaryOp);
 		}
 
 		protected override void WriteLineComment(string text)
@@ -42,6 +58,38 @@ namespace EinCompiler.BackEnds
 			WriteLine ("#include <stdint.h>");
 			WriteLine ("#include <stddef.h>");
 
+			foreach (var c in module.Constants) {
+
+				Write ("const ");
+				Write (GetTypeName (c.Type));
+				Write (" ");
+				Write (c.Name);
+				Write (" = ");
+				Write (c.InitialValue.Value.ToString ());
+				WriteLine (";");
+
+			}
+
+			foreach (var v in module.Variables) {
+
+				if (v.Storage.HasFlag (StorageModifier.Static)) {
+					Write ("static ");
+				}
+				Write (GetTypeName (v.Type));
+				Write (" ");
+				Write (v.Name);
+				if (v.InitialValue != null) {
+					Write (" = ");
+					Write (v.InitialValue.Value.ToString ());
+				}
+				WriteLine (";");
+
+			}
+			foreach (var func in module.Functions) {
+
+				WriteHeader (func);
+				WriteLine (";");
+			}
 
 			foreach (var func in module.Functions) {
 
@@ -54,7 +102,16 @@ namespace EinCompiler.BackEnds
 					DecreaseIndentation ();
 					WriteLine ("}");
 				} else {
-					WriteBody (func.Body);
+					WriteLine ("{");
+					IncreaseIndentation ();
+
+					foreach (var var in func.Locals) {
+						WriteLine ("{0} {1};", GetTypeName (var.Type), var.Name);
+					}
+
+					WriteBody (func.Body, true);
+					DecreaseIndentation ();
+					WriteLine ("}");
 				}
 			}
 		}
@@ -78,21 +135,58 @@ namespace EinCompiler.BackEnds
 			Write (")");
 		}
 
-		private void WriteBody(BodyDescription body)
+		private void WriteBody(BodyDescription body, bool suppressBrackets = false)
 		{
-			WriteLine ("{");
-			IncreaseIndentation ();
+			if (!suppressBrackets) {
+				WriteLine ("{");
+				IncreaseIndentation ();
+			}
 			foreach (var i in body) {
 				writeInstruction.Invoke (i, null);
 			}
-			DecreaseIndentation ();
-			WriteLine ("}");
+			if (!suppressBrackets) {
+				DecreaseIndentation ();
+				WriteLine ("}");
+			}
 		}
 
 		private void WriteExpressionInstruction(ExpressionInstruction instr, object obj)
 		{
 			writeExpression.Invoke (instr.Expression, obj);
 			WriteLine (";");
+		}
+
+		private void WriteReturn(ReturnInstruction instr, object obj)
+		{
+			Write ("return ");
+			writeExpression.Invoke (instr.Expression, obj);
+			WriteLine(";");
+		}
+
+		private void WriteIf(ConditionalInstruction instr, object obj)
+		{
+			Write ("if(");
+			writeExpression.Invoke (instr.Condition, obj);
+			WriteLine (")");
+			WriteBody (instr.TrueBody);
+			if (instr.FalseBody != null) {
+				WriteLine ("else");
+				WriteBody (instr.FalseBody);
+			}
+			WriteLine();
+		}
+
+		private void WriteLoop(LoopInstruction instr, object obj)
+		{
+			Write ("while(");
+			writeExpression.Invoke (instr.Condition, obj);
+			WriteLine (")");
+			WriteBody (instr.Body);
+		}
+
+		private void WriteBreak(BreakLoopInstruction instr, object obj)
+		{
+			WriteLine ("break;");
 		}
 
 		private void WriteAssignment(AssignmentExpression expr, object obj)
@@ -131,6 +225,22 @@ namespace EinCompiler.BackEnds
 			Write ("[");
 			writeExpression.Invoke (expr.Index, obj);
 			Write ("]");
+		}
+
+		private void WriteBinaryOp(BinaryOperatorExpression expr, object obj)
+		{
+			Write ("(");
+			writeExpression.Invoke (expr.LeftHandSide, obj);
+			Write (")");
+			string val;
+			if (binaryOperators.TryGetValue (expr.Operator, out val)) {
+				Write (val);
+			} else {
+				throw new NotSupportedException ($"Operator {expr.Operator} is not supported.");
+			}
+			Write ("(");
+			writeExpression.Invoke (expr.RightHandSide, obj);
+			Write (")");
 		}
 	}
 }
