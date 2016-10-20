3145a54f5f607f93eb7ab740305680e5839bd48b
Felix Queißner <git@mq32.de>
2016-10-20 12:24:40 +0200

    Restarts implementation of c code backend.

--- EinCompiler/BackEnds/CCodeBackEnd.cs
+++ EinCompiler/BackEnds/CCodeBackEnd.cs
@@ -9,51 +9,30 @@ namespace EinCompiler.BackEnds
 {
 	public sealed class CCodeBackEnd : BackEnd
 	{
+		readonly Dispatcher<InstructionDescription, object> writeInstruction = new Dispatcher<InstructionDescription, object>();
+		readonly Dispatcher<Expression, object> writeExpression = new Dispatcher<Expression, object>();
+
+		public CCodeBackEnd()
+		{
+			writeInstruction.Register<ExpressionInstruction> (WriteExpressionInstruction);
+
+
+
+			writeExpression.Register<AssignmentExpression> (WriteAssignment);
+			writeExpression.Register<VariableExpression> (WriteVariable);
+			writeExpression.Register<LiteralExpression> (WriteLiteral);
+			writeExpression.Register<FunctionCallExpression> (WriteCall);
+			writeExpression.Register<IndexerExpression> (WriteIndexer);
+		}
+
 		protected override void WriteLineComment(string text)
 		{
 			WriteLine("// {0}", text);
 		}
 
-		private readonly Dictionary<TypeDescription, string> typeNames = new Dictionary<TypeDescription, string>();
-
-		static string TypeName(TypeDescription type)
+		private string GetTypeName(TypeDescription type)
 		{
-			if (type == Types.Void) {
-				return "void";
-			}
-
-			if (type is IntegerType) {
-				string name = "";
-				var itype = (IntegerType)type;
-				if (!itype.IsSigned) {
-					name += "u";
-				}
-				name += "int";
-				switch (itype.Size) {
-				case 1:
-					name += "8";
-					break;
-				case 2:
-					name += "16";
-					break;
-				case 4:
-					name += "32";
-					break;
-				case 8:
-					name += "64";
-					break;
-				default:
-					throw new NotSupportedException ();
-				}
-				name += "_t";
-				return name;
-			}
-			if (type is ArrayType) {
-				var atype = (ArrayType)type;
-				return TypeName(atype.ElementType) + $"[{atype.Length}]";
-			}
-
-			throw new NotSupportedException();
+			return "int";
 		}
 
 		protected override void Generate(ModuleDescription module)
@@ -63,183 +42,95 @@ namespace EinCompiler.BackEnds
 			WriteLine ("#include <stdint.h>");
 			WriteLine ("#include <stddef.h>");
 
-			WriteLineComment ("Types:");
-			foreach (var type in module.Types) {
 
-				typeNames [type.Value] = TypeName (type.Value);
+			foreach (var func in module.Functions) {
 
-			}
-			WriteLineComment("Constants:");
-			foreach (var @const in module.Constants)
-			{
-				WriteConstant(@const);
-			}
-
-			WriteLine();
-
-			WriteLineComment("Variables:");
-			foreach (var var in module.Variables)
-			{
-				WriteVariable(var);
-			}
-
-			WriteLine();
-
-			WriteLineComment("Function Prototypes:");
-			foreach (var func in module.Functions)
-			{
-				WriteFunctionHeader(func);
-				WriteLine(";");
-			}
-
-			WriteLine();
-
-			WriteLineComment("Functions:");
-			foreach (var func in module.Functions)
-			{
-				WriteFunctionHeader(func);
+				WriteHeader (func);
+				WriteLine ();
 				if (func.NakedBody != null) {
 					WriteLine ("{");
+					IncreaseIndentation ();
 					WriteLine (func.NakedBody);
+					DecreaseIndentation ();
 					WriteLine ("}");
-				} else { 
-					WriteLine ();
-					WriteBlock (func.Body);
-					WriteLine ();
+				} else {
+					WriteBody (func.Body);
 				}
 			}
 		}
 
-		private void WriteBlock(BodyDescription body)
+		private void WriteHeader(FunctionDescription func)
 		{
-			WriteLine("{");
-			IncreaseIndentation();
+			Write (GetTypeName (func.ReturnType));
+			Write (" ");
+			Write (func.Name);
+			Write ("(");
 
-			foreach (var instr in body)
-			{
-				WriteInstruction(instr);
+			for (int i = 0; i < func.Parameters.Count; i++) {
+				if (i > 0)
+					Write (", ");
+				var p = func.Parameters [i];
+				Write (GetTypeName (p.Type));
+				Write (" ");
+				Write (p.Name);
 			}
 
-			DecreaseIndentation();
-			WriteLine("}");
+			Write (")");
 		}
 
-		private void WriteInstruction(InstructionDescription instr)
+		private void WriteBody(BodyDescription body)
 		{
-			if (instr is ExpressionInstruction) {
-				WriteExpression (((ExpressionInstruction)instr).Expression);
-				WriteLine (";");
-			} else if (instr is ConditionalInstruction) {
-				var condition = ((ConditionalInstruction)instr).Condition;
-				var trueBody = ((ConditionalInstruction)instr).TrueBody;
-				var falseBody = ((ConditionalInstruction)instr).FalseBody;
-
-				Write ("if (");
-				WriteExpression (condition);
-				WriteLine (")");
-				WriteBlock (trueBody);
-				if (falseBody != null) {
-					WriteLine ("else");
-					WriteBlock (falseBody);
-				}
-			} else if (instr is LoopInstruction) {
-				var condition = ((LoopInstruction)instr).Condition;
-				var body = ((LoopInstruction)instr).Body;
-				Write ("while(");
-				WriteExpression (condition);
-				WriteLine (")");
-				WriteBlock (body);
-			} else if (instr is ReturnInstruction) {
-				var expr = ((ReturnInstruction)instr).Expression;
-				Write ("return ");
-				WriteExpression (((ReturnInstruction)instr).Expression);
-				WriteLine (";");
-			} else if (instr is NopInstruction) {
-				WriteLine (";");
-			} else if (instr is BreakLoopInstruction) {
-				WriteLine ("break;");
-			} else {
-				throw new NotSupportedException ($"Instruction {instr.GetType().Name} is not supported yet.");
+			WriteLine ("{");
+			IncreaseIndentation ();
+			foreach (var i in body) {
+				writeInstruction.Invoke (i, null);
 			}
+			DecreaseIndentation ();
+			WriteLine ("}");
 		}
 
-		private void WriteExpression(Expression expression)
+		private void WriteExpressionInstruction(ExpressionInstruction instr, object obj)
 		{
-			if (expression is AssignmentExpression) {
-				var ass = (AssignmentExpression)expression;
+			writeExpression.Invoke (instr.Expression, obj);
+			WriteLine (";");
+		}
 
-				WriteExpression (ass.Target);
-				Write (" = ");
-				WriteExpression (ass.Source);
-			} else if (expression is BinaryOperatorExpression) {
-				var bin = (BinaryOperatorExpression)expression;
+		private void WriteAssignment(AssignmentExpression expr, object obj)
+		{
+			writeExpression.Invoke (expr.Target, obj);
+			Write (" = ");
+			writeExpression.Invoke (expr.Source, obj);
+		}
 
-				WriteExpression (bin.LeftHandSide);
-				switch (bin.Operator) {
-				case BinaryOperator.Addition:
-					Write (" + ");
-					break;
-				case BinaryOperator.Subtraction:
-					Write (" - ");
-					break;
-				case BinaryOperator.Multiplication:
-					Write (" * ");
-					break;
-				case BinaryOperator.Division:
-					Write (" / ");
-					break;
-				case BinaryOperator.EuclideanDivision:
-					Write (" % ");
-					break;
-				default:
-					throw new NotSupportedException ();
-				}
-				WriteExpression (bin.RightHandSide);
-			} else if (expression is FunctionCallExpression) {
-				var call = (FunctionCallExpression)expression;
+		private void WriteVariable(VariableExpression expr, object obj)
+		{
+			Write (expr.Variable.Name);
+		}
 
-				Write ("{0}(", call.Function.Name);
-				
-				for (int i = 0; i < call.Arguments.Length; i++) {
-					if (i > 0)
-						Write (", ");
-					WriteExpression (call.Arguments [i]);
-				}
+		private void WriteLiteral(LiteralExpression expr, object obj)
+		{
+			Write (expr.Literal);
+		}
 
-				Write (")");
-			} else if (expression is LiteralExpression) {
-				Write (((LiteralExpression)expression).Literal);
-			} else if (expression is VariableExpression) {
-				Write (((VariableExpression)expression).Variable.Name);
-			} else if (expression is IndexerExpression) {
-				var expr = (IndexerExpression)expression;
-				Write ("{0}[", expr.Array.Name);
-				WriteExpression (expr.Index);
-				Write ("]");
-			} else {
-				throw new NotSupportedException ($"Expression {expression.GetType().Name} is not supported yet.");
+		private void WriteCall(FunctionCallExpression expr, object obj)
+		{
+			Write (expr.Function.Name);
+			Write ("(");
+			for (int i = 0; i < expr.Arguments.Length; i++) {
+				if (i > 0)
+					Write (", ");
+				writeExpression.Invoke (expr.Arguments [i], obj);
 			}
+
+			Write (")");
 		}
 
-		private void WriteFunctionHeader(FunctionDescription func)
+		private void WriteIndexer(IndexerExpression expr, object obj)
 		{
-			Write("{0} {1}({2})",
-				typeNames[func.ReturnType],
-				func.Name,
-				String.Join(", ", func.Parameters.Select(p => $"{typeNames[p.Type]} {p.Name}")));
-		}
-
-		private void WriteConstant(ConstantDescription @const)
-		{
-			WriteLine("const {0} {1} = {2};", typeNames[@const.Type], @const.Name, @const.InitialValue.GetString());
-		}
-
-		private void WriteVariable(VariableDescription var)
-		{
-			if (var.InitialValue != null)
-				WriteLine("{0} {1} = {2};", typeNames[var.Type], var.Name, var.InitialValue.GetString());
-			else
-				WriteLine("{0} {1};", typeNames[var.Type], var.Name);
+			Write (expr.Array.Name);
+			Write ("[");
+			writeExpression.Invoke (expr.Index, obj);
+			Write ("]");
 		}
 	}
 }
---
+++ EinCompiler/Dispatcher.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EinCompiler
+{
+	public sealed class Dispatcher<TObject, TArgs>
+	{
+		public delegate void Function(TObject obj, TArgs args);
+
+		public delegate void DispatchFunction<T>(T obj, TArgs args)
+			where T : TObject;
+
+		private readonly Dictionary<Type, Function> handlers = new Dictionary<Type, Function>();
+
+		public Dispatcher()
+		{
+
+		}
+
+		public void Register<T>(DispatchFunction<T> func)
+			where T : TObject
+		{
+			handlers [typeof(T)] = (o,a) => func((T)o, a);
+		}
+
+		public void Invoke<T>(T obj, TArgs args) 
+			where T : TObject
+		{
+			var type = obj.GetType ();
+			while(type != null && type != typeof(TObject))
+			{
+				Function func;
+				if (handlers.TryGetValue (type, out func)) {
+					func.Invoke (obj, args);
+					return;
+				}
+				type = type.BaseType;
+			}
+			throw new InvalidOperationException($"No handler registered for {obj.GetType().Name}.");
+		}
+	}
+}
+
